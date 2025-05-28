47c0aaee68ffcdbee6b75503105ddfee4c9f1b98
Eli Franklin <foreverblue173@gmail.com>
2025-05-28 17:50:02 -0600

    fixed a lil thingy

--- resources/js/apiEndpoints/config.js
+++ resources/js/apiEndpoints/config.js
@@ -4,5 +4,5 @@ window.API_LINK = API_LINK
 
 //PATHS - ALL PATHS SHOULD BE FROM ROOT
 const LOGIN_PAGE = `games/slots/accountSettings.html`
-
+window.LOGIN_PAGE = LOGIN_PAGE
 //<script src = "resources/js/apiEndpoints/config.js">//imports API configs</script>
