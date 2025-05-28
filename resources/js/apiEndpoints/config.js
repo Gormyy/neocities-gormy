d50c41df7b2ebaf228c92e9a02977b5545d1397f
Eli Franklin <foreverblue173@gmail.com>
2025-05-28 16:33:05 -0600

    Made access denied page

--- resources/js/apiEndpoints/config.js
+++ resources/js/apiEndpoints/config.js
@@ -2,4 +2,7 @@ const API_LINK = `https://gormysecret.onrender.com`
 
 window.API_LINK = API_LINK
 
+//PATHS - ALL PATHS SHOULD BE FROM ROOT
+const LOGIN_PAGE = `games/slots/accountSettings.html`
+
 //<script src = "resources/js/apiEndpoints/config.js">//imports API configs</script>
