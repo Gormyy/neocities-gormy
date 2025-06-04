9cb27dca5d737cbe0102a5daf544c4d5b5e53ab4
Eli Franklin <foreverblue173@gmail.com>
2025-06-04 02:53:24 -0400

    fixing times

--- resources/js/miscStuff/fixDatesForBlogs.js
+++ resources/js/miscStuff/fixDatesForBlogs.js
@@ -1,8 +1,9 @@
 function fixDatesForBlogs(blogs){
     for (let blog of blogs){
         const d = new Date(blog.created_at)
-        blog.date = `${d.getFullYear()}-${d.getMonth().toString().padStart(2, '0')}-${d.getDay().toString().padStart(2, '0')}`
-         console.log(blog)
+        console.log(d)
+        blog.date = `${d.getFullYear()}-${(d.getMonth() + 1).toString().padStart(2, '0')}-${(d.getDay() + 1).toString().padStart(2, '0')}`
+        console.log(blog.date)
     }
     return blogs
 }
